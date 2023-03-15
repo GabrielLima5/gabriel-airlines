75917ba60aedd683036405b2f3596330d0dd5d19
Gabriel Lima <gabriellpbiel@hotmail.com>
2023-03-15 18:54:48 -0300

    adicionando margem em um ícone

---
+++ Destinos Gabriel Airlines/assets/js/script.js
@@ -0,0 +1,8 @@
+// RESPONSIVE SETTINGS
+document.querySelector('.open-menu').addEventListener('click', e => {
+    document.querySelector('header .menu').classList.add('open');
+});
+
+document.querySelector('.close-menu').addEventListener('click', e => {
+    document.querySelector('header .menu').classList.remove('open');
+});
