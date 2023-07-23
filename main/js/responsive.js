c5ad6f983a3ce7dc1707547ad42ca7ecb6a499ec
Gabriel Lima <gabriellpbiel@hotmail.com>
2023-07-23 13:07:16 -0300

    corrigindo erros de metadados e user experience

--- main/js/responsive.js
+++ main/js/responsive.js
@@ -1,20 +1,27 @@
 class ResponsiveSettings{
-    constructor(openButton, closeButton, menu){
+    constructor(openButton, closeButton, menu, fadeEl){
         this.openButton = openButton
         this.closeButton = closeButton
         this.menu = menu
+        this.fade = fadeEl
     }
 
     settings(){
         this.openButton.addEventListener('click', e => {
             this.menu.classList.add('open')
+            this.fade.classList.remove('hide')
         });
 
         this.closeButton.addEventListener('click', e => {
             this.menu.classList.remove('open')
+            this.fade.classList.add('hide')
         })
     }
 }
 
-const responsive = new ResponsiveSettings(document.querySelector('.open-menu'), document.querySelector('.close-menu'), document.querySelector('header .menu'))
+const responsive = new ResponsiveSettings(
+    document.querySelector('.open-menu'), 
+    document.querySelector('.close-menu'), 
+    document.querySelector('header .menu'), 
+    document.querySelector('.fade'))
 window.onload = responsive.settings()
